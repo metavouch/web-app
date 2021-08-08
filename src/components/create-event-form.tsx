22c2f9c9387e919b78eeb815c830d86685d756d2
hexcowboy <hex@cowboy.dev>
2021-08-08 15:27:33 -0500

    create forms that upload to ipfs

--- src/components/create-event-form.tsx
+++ src/components/create-event-form.tsx
@@ -1,41 +1,81 @@
-import { url } from 'inspector';
-import React from 'react';
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { create } from "ipfs-http-client";
 
-const EventForm = () => {
-    return <form>
-        <input type="text" name="title" id="title" placeholder="Event Title" />
-        <textarea name="description" id="description" placeholder="Event Description"></textarea>
-        <input type="datetime-local" name="datetime" id="datetime" />
-    </form>;
+interface EventInput {
+  title: string;
+  description: string;
+  location: string;
+  datetime: string;
 }
 
-// contract Event {
-//     int ticketId;
-//     string url;
+const EventForm: React.FC = () => {
+  const client = create({ url: "https://ipfs.infura.io:5001/api/v0" });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EventInput>();
 
-//     Event() {
-//         url = "ipfs://u3gh8u4gi23gu4g2u3iu4g/{id}.json"
-//     }
-// }
+  const onSubmit: SubmitHandler<EventInput> = async (data) => {
+    console.log(data);
+    try {
+      // Using the 'new' keyword makes this a JavaScript string and
+      // not a TypeScript string, which is required for IPFS `add`.
+      const jsonData = JSON.stringify(data);
+      const added = await client.add(jsonData);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      console.log(url);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  };
 
-// {
-//     "title": "General Admissioin",
-//         "type": "ticket",
-//             "description": "General Admissioin gets access to all stages",
-//                 "properties": {
-//         "name": {
-//             "type": "string",
-//                 "description": "Identifies the event to which this NFT represents"
-//         },
-//         "description": {
-//             "type": "string",
-//                 "description": "Describes the event to which this NFT represents"
-//         },
-//         "image": {
-//             "type": "string",
-//                 "description": "ipfs://QmRAQB6faCyidP37UdDnjFY5vQuiBrcqdyoW1CuDgwxkD4"
-//         }
-//     }
-// }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="title">
+        Event Title {errors.title && <span className="error">Required</span>}
+      </label>
+      <input
+        {...register("title", { required: true })}
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Event Title"
+      />
+      <label htmlFor="description">
+        Event Description{" "}
+        {errors.description && <span className="error">Required</span>}
+      </label>
+      <textarea
+        {...register("description", { required: true })}
+        name="description"
+        id="description"
+        placeholder="Event Description"
+      ></textarea>
+      <label htmlFor="location">
+        Event Location{" "}
+        {errors.location && <span className="error">Required</span>}
+      </label>
+      <input
+        {...register("location", { required: true })}
+        type="text"
+        name="location"
+        id="location"
+        placeholder="Event Location"
+      />
+      <label htmlFor="datetime">
+        Event Time {errors.datetime && <span className="error">Required</span>}
+      </label>
+      <input
+        {...register("datetime", { required: true })}
+        type="datetime-local"
+        name="datetime"
+        id="datetime"
+      />
+      <button type="submit">Create</button>
+    </form>
+  );
+};
 
 export default EventForm;
