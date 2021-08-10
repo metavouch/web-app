0d987fb6e1bc1ec372886234d1e7eadc19f954c1
hexcowboy <hex@cowboy.dev>
2021-08-10 15:30:36 -0500

    enable optional wallet providers #22 #16

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -1,16 +1,47 @@
+import React from "react";
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWallet } from "@fortawesome/free-solid-svg-icons";
 import { useMoralis } from "react-moralis";
+
 import "../stylesheets/forms.scss";
+import "../stylesheets/modals.scss";
+import metamaskLogo from "../images/metamask-logo.svg";
+import walletconnectLogo from "../images/walletconnect-circle-blue.svg";
 
-function ConnectButton() {
-  const { authenticate, isAuthenticated } = useMoralis();
+const ConnectButton: React.FC = () => {
+  const { authenticate, logout, isAuthenticated } = useMoralis();
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
-  const buttonContent = isAuthenticated ? (
-    <button>View Wallet</button>
-  ) : (
-    <button onClick={() => authenticate()}>Connect Wallet</button>
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+    <button onClick={openModal}>
+      <FontAwesomeIcon icon={faWallet} />
+    </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <button onClick={() => authenticate()}>
+          <img src={metamaskLogo} alt="Connect with MetaMask" />
+        </button>
+        <button onClick={() => authenticate({ provider: "walletconnect" })}>
+          <img src={walletconnectLogo} alt="Connect with WalletConnect" />
+        </button>
+      </Modal>
+    </>
   );
-
-  return buttonContent;
-}
+};
 
 export default ConnectButton;
